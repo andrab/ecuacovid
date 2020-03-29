afc49af5a11513e792aff7e61fb67cd8cbb2cf7e
Andrés N. Robalino <andres@androbtech.com>
2020-03-28 20:40:21 -0500

    Muertes sincronizadas hasta Marzo 23, 2020.

--- notas/muertes.md
+++ notas/muertes.md
@@ -7,6 +7,24 @@
 | 20/03/2020 | Guayas    | Guayaquil  |
 | 20/03/2020 | -         | -          |
 | 20/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 21/03/2020 | -         | -          |
+| 22/03/2020 | Cotopaxi  | La Maná    |
+| 22/03/2020 | Guayas    | Daule      |
+| 22/03/2020 | Guayas    | Salitre    |
+| 22/03/2020 | Guayas    | Guayaquil  |
 
 # Referencias
 
@@ -24,5 +42,13 @@
 * _"Coronavirus: 4 fallecidos, 260 casos positivos y 3 pacientes recuperados en Ecuador"_ ([leer más](https://www.expreso.ec/actualidad/coronavirus-4-fallecidos-260-casos-positivos-3-pacientes-recuperados-ecuador-7293.html))
 
 ## Marzo 20, 2020
-* Info Riesgos Ecuador Número 014 (_[ver](../fuentes/SNGRE_014.pdf)_) y 015 [_[ver](../fuentes/SNGRE_015.pdf)_).
+* Info Riesgos Ecuador Número 014 (_[ver](../fuentes/SNGRE_014.pdf)_) y 015 (_[ver](../fuentes/SNGRE_015.pdf)_).
 * _"10:00 Informe N° 14 Coronavirus en Ecuador: 367 personas contagiadas, hay un quinto fallecido"_ ([leer más](https://www.eluniverso.com/noticias/2020/03/20/nota/7789119/coronavirus-ecuador-367-personas-contagiadas-hasta-hoy))
+
+## Marzo 21, 2020
+* Info Riesgos Ecuador Número 018 (_[ver](../fuentes/SNGRE_018.pdf)_).
+* _"Casos de Coronavirus en Ecuador marzo 22, 11H00: 789 contagiados y 14 fallecidos"_ ([leer más])(https://www.eluniverso.com/noticias/2020/03/22/nota/7791375/casos-coronavirus-ecuador-marzo-22-11h00-789-contagiados))
+
+## Marzo 22, 2020
+* Info Riesgos Ecuador Número 019 (_[ver](../fuentes/SNGRE_019.pdf)_).
+* _"18 fallecidos y 981 contagios de coronavirus en Ecuador"_ ([leer más](https://www.primicias.ec/noticias/sociedad/coronavirus-ecuador-emergencia-sanitaria/))
--- spec/ecuacovid/criterios.rb
+++ spec/ecuacovid/criterios.rb
@@ -9,6 +9,23 @@ class Criterios
         def teniendo(sujeto)
           Teniendo.new(sujeto)
         end
+
+        def teniendo_provincias_totales(sujeto)
+          TeniendoProvincias.new(sujeto)
+        end
+      end
+    end
+
+    class TeniendoProvincias
+      def initialize(cantidad)
+        @cantidad = cantidad
+      end
+
+      def de(provincia)
+        {
+          de_las_provincias_teniendo: Hash[provincia, @cantidad],
+          teniendo_sin_clasificar: 0
+        }
       end
     end
 
@@ -17,15 +34,15 @@ class Criterios
         @cantidad = cantidad
       end
 
-      def y_tiene(total)
-        @tendra = total
+      def y_tiene(*totales)
+        @tendra = totales
         self
       end
       
-      def de(provincia)
+      def de(*provincias)
         {
-          de_las_provincias_teniendo: Hash[provincia, @tendra],
-             teniendo_sin_clasificar: @sin
+          de_las_provincias_teniendo: provincias.zip(@tendra),
+             teniendo_sin_clasificar: @cantidad
         }
       end
     end
@@ -34,9 +51,13 @@ class Criterios
       Condicion.teniendo(total_casos_sin_origen)
     end
 
+    def con(total_casos_de_provincia)
+      Condicion.teniendo_provincias_totales(total_casos_de_provincia)
+    end
+
     class ::Integer
       def muertes
-        {:muertes => self}
+        Hash[:muertes, self]
       end
     end
 
@@ -48,31 +69,34 @@ class Criterios
 
     def positivas
       #"──INFORME SNGRE──┬───FECHA────┬───────────────────────ACEPTACION────────────────────────────────────┬"
-      [[  :SNGRE_xxx     ,"25/03/2020", {casos: 1211, cantones_ingresados: 77, cantones_sin_ingresar: 144}],
-       [  :SNGRE_xxx     ,"24/03/2020", {casos: 1082, cantones_ingresados: 68, cantones_sin_ingresar: 153}],
-       [  :SNGRE_xxx     ,"23/03/2020", {casos:  981, cantones_ingresados: 58, cantones_sin_ingresar: 163}],
-       [  :SNGRE_xxx     ,"22/03/2020", {casos:  789, cantones_ingresados: 51, cantones_sin_ingresar: 170}],
-       [  :SNGRE_xxx     ,"21/03/2020", {casos:  532, cantones_ingresados: 43, cantones_sin_ingresar: 178}],
-       [  :SNGRE_xxx     ,"20/03/2020", {casos:  426, cantones_ingresados: 37, cantones_sin_ingresar: 184}],
-       [  :SNGRE_013     ,"19/03/2020", {casos:  260, cantones_ingresados: 26, cantones_sin_ingresar: 195}],
-       [  :SNGRE_011     ,"18/03/2020", {casos:  168, cantones_ingresados: 16, cantones_sin_ingresar: 205}],
-       [  :SNGRE_009     ,"17/03/2020", {casos:  111, cantones_ingresados: 15, cantones_sin_ingresar: 206}],
-       [  :SNGRE_007     ,"16/03/2020", {casos:   58, cantones_ingresados: 12, cantones_sin_ingresar: 209}],
-       [  :SNGRE_005     ,"15/03/2020", {casos:   37, cantones_ingresados: 11, cantones_sin_ingresar: 210}],
-       [  :SNGRE_003     ,"14/03/2020", {casos:   28, cantones_ingresados: 10, cantones_sin_ingresar: 211}],
-       [  :SNGRE_002     ,"13/03/2020", {casos:   23, cantones_ingresados:  8, cantones_sin_ingresar: 213}]]
+      [[  :SNGRE_xxx     ,"25/03/2020", {casos: 1211, cantones_ingresados: 77,   cantones_sin_ingresar: 144}],
+       [  :SNGRE_xxx     ,"24/03/2020", {casos: 1082, cantones_ingresados: 68,   cantones_sin_ingresar: 153}],
+       [  :SNGRE_xxx     ,"23/03/2020", {casos:  981, cantones_ingresados: 58,   cantones_sin_ingresar: 163}],
+       [  :SNGRE_xxx     ,"22/03/2020", {casos:  789, cantones_ingresados: 51,   cantones_sin_ingresar: 170}],
+       [  :SNGRE_xxx     ,"21/03/2020", {casos:  532, cantones_ingresados: 43,   cantones_sin_ingresar: 178}],
+       [  :SNGRE_xxx     ,"20/03/2020", {casos:  426, cantones_ingresados: 37,   cantones_sin_ingresar: 184}],
+       [  :SNGRE_013     ,"19/03/2020", {casos:  260, cantones_ingresados: 26,   cantones_sin_ingresar: 195}],
+       [  :SNGRE_011     ,"18/03/2020", {casos:  168, cantones_ingresados: 16,   cantones_sin_ingresar: 205}],
+       [  :SNGRE_009     ,"17/03/2020", {casos:  111, cantones_ingresados: 15,   cantones_sin_ingresar: 206}],
+       [  :SNGRE_007     ,"16/03/2020", {casos:   58, cantones_ingresados: 12,   cantones_sin_ingresar: 209}],
+       [  :SNGRE_005     ,"15/03/2020", {casos:   37, cantones_ingresados: 11,   cantones_sin_ingresar: 210}],
+       [  :SNGRE_003     ,"14/03/2020", {casos:   28, cantones_ingresados: 10,   cantones_sin_ingresar: 211}],
+       [  :SNGRE_002     ,"13/03/2020", {casos:   23, cantones_ingresados:  8,   cantones_sin_ingresar: 213}]]
     end
 
     def muertes
-      #"──────────────────┬──FECHA───┬────────────────────────ACEPTACION───────────────────────────────────┬"
-      [[                  "20/03/2020",             7.muertes.y(sin_provincia(2).y_tiene(4).de("Guayas"))],  
-       [                  "19/03/2020", {muertes:   4,       de_las_provincias_teniendo: {"Manabí" => 1}}],
-       [                  "18/03/2020", {muertes:   3,       de_las_provincias_teniendo: {"Guayas" => 3}}],
-       [                  "17/03/2020", {muertes:                                                      2}],
-       [                  "16/03/2020", {muertes:                                                      2}],
-       [                  "15/03/2020", {muertes:                                                      2}],
-       [                  "14/03/2020", {muertes:                                                      2}],
-       [                  "13/03/2020", {muertes:   2,       de_las_provincias_teniendo: {"Guayas" => 2}}]]
+      #"────┬──FECHA───┬──────────────────────────────────ACEPTACION────────────────────────────────────────┬"
+      [[    "23/03/2020",              18.muertes.y(sin_provincia(9).y_tiene(3,1).de("Guayas", "Cotopaxi"))],
+       [    "22/03/2020",                                                                        14.muertes],
+       [    "21/03/2020",                                                                         7.muertes],                   
+       [    "20/03/2020",                             7.muertes.y(sin_provincia(2).y_tiene(4).de("Guayas"))],  
+       [    "19/03/2020",                                                  4.muertes.y(con(1).de("Manabí"))],
+       [    "18/03/2020",                                                  3.muertes.y(con(3).de("Guayas"))],
+       [    "17/03/2020",                                                                         2.muertes],
+       [    "16/03/2020",                                                                         2.muertes],
+       [    "15/03/2020",                                                                         2.muertes],
+       [    "14/03/2020",                                                                         2.muertes],
+       [    "13/03/2020",                                                  2.muertes.y(con(2).de("Guayas"))]]
     end
   end
 end
