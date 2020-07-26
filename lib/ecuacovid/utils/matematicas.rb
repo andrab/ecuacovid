12cd9608ac9a9fafb0ff66e07f6d72999e74b883
Andrés N. Robalino <andres@androbtech.com>
2020-07-26 14:16:58 -0500

    Defunciones Generales Años: 2015, 2016, 2017, 2018, y 2019.
    Para cada año contiene datos estructurados por cantones, provincias, cantones por mes, y provincias por mes.
    
    Adicionalmente, para provincias se proporcionan tanto por fallecimiento de provincia de residencia y lugar de fallecimiento.
    
    Solamente hay fallecimientos por provincia de residencia por lo que únicamente los cantones son por lugar de fallecimiento y
    no hay fallecimientos por cantón de residencia.

---
+++ lib/ecuacovid/utils/matematicas.rb
@@ -0,0 +1,35 @@
+module Ecuacovid
+  module Utils
+    class ::Array
+      def mediana
+        Mediana.para(self)
+      end
+    end
+
+    class Mediana
+      class << self
+        def para(datum)
+          data = new(datum)
+          data.mediana
+        end
+      end
+
+      def initialize(datum)
+        @datum = datum.sort
+      end
+
+      def par?
+        @datum.size % 2 == 0
+      end
+
+      def mediana
+        return @datum[mitad].to_f if !par?
+        @datum[((mitad - 1)..mitad)].sum / 2.0
+      end
+
+      def mitad
+        @datum.size / 2
+      end
+    end
+  end
+end
