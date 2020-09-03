b1055541f92377d4d96ce6c5954939a9a575bd90
Andrés N. Robalino <andres@androbtech.com>
2020-09-03 01:58:03 -0500

    Complemento Promedio

--- lib/ecuacovid/utils/matematicas.rb
+++ lib/ecuacovid/utils/matematicas.rb
@@ -1,8 +1,29 @@
 module Ecuacovid
   module Utils
     class ::Array
+      def promedio
+        Promedio.para(self)
+      end
+
       def mediana
         Mediana.para(self)
+      end
+    end
+
+    class Promedio
+      class << self
+        def para(datum)
+          data = new(datum)
+          data.promedio
+        end
+      end
+
+      def initialize(datum)
+        @datum = datum
+      end
+
+      def promedio
+        @datum.map(&:to_f).sum / @datum.size.to_f
       end
     end
 
