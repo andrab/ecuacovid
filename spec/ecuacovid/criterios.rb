4c6e553131a2bbdebc59add2c2ac41ab3deabb4e
Francisco Hurtado <panchurtado@gmail.com>
2021-07-10 15:31:09 -0500

    Informe SNGRE No. 499

--- spec/ecuacovid/criterios.rb
+++ spec/ecuacovid/criterios.rb
@@ -487,7 +487,8 @@ class Criterios
         [  :SNGRE_495_08H   ,"06/07/2021", {casos: 463951, cantones_ingresados: 221, sin_ingresar:   0}],
         [  :SNGRE_496_08H   ,"07/07/2021", {casos: 465029, cantones_ingresados: 221, sin_ingresar:   0}],
         [  :SNGRE_497_08H   ,"08/07/2021", {casos: 465878, cantones_ingresados: 221, sin_ingresar:   0}],
-        [  :SNGRE_498_08H   ,"09/07/2021", {casos: 467073, cantones_ingresados: 221, sin_ingresar:   0}]
+        [  :SNGRE_498_08H   ,"09/07/2021", {casos: 467073, cantones_ingresados: 221, sin_ingresar:   0}],
+        [  :SNGRE_499_08H   ,"10/07/2021", {casos: 467976, cantones_ingresados: 221, sin_ingresar:   0}]
       ]
       .reverse
       .seleccionable
@@ -496,6 +497,7 @@ class Criterios
 
     def muertes
       [#"──INFORME  SNGRE───┬───FECHA────┬────────────────────────────ACEPTACION──────────────────────────"
+        [  :SNGRE_499_08H   ,"10/07/2021", { muertes: 16111, provincias_ingresadas: 24, sin_ingresar:  0}],
         [  :SNGRE_498_08H   ,"09/07/2021", { muertes: 16103, provincias_ingresadas: 24, sin_ingresar:  0}],
         [  :SNGRE_497_08H   ,"08/07/2021", { muertes: 16071, provincias_ingresadas: 24, sin_ingresar:  0}],
         [  :SNGRE_496_08H   ,"07/07/2021", { muertes: 16033, provincias_ingresadas: 24, sin_ingresar:  0}],
@@ -2491,7 +2493,8 @@ class Criterios
         [  :SNGRE_495_08H   ,"06/07/2021", {probables: 5688, total: 21708}],
         [  :SNGRE_496_08H   ,"07/07/2021", {probables: 5695, total: 21728}],
         [  :SNGRE_497_08H   ,"08/07/2021", {probables: 5697, total: 21768}],
-        [  :SNGRE_498_08H   ,"09/07/2021", {probables: 5700, total: 21803}]
+        [  :SNGRE_498_08H   ,"09/07/2021", {probables: 5700, total: 21803}],
+        [  :SNGRE_499_08H   ,"10/07/2021", {probables: 5703, total: 21814}]
       ]
       .reverse
       .seleccionable
@@ -2538,12 +2541,12 @@ class Criterios
     ];
 
     [
-     1579458,
-       16103,
-        5700,
-       21803,
-      467073,
-     1057772,
+     1581643,
+       16111,
+        5703,
+       21814,
+      467976,
+     1059628,
        48969,
          650,
          427
