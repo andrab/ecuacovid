f37eb3c23067e05f090a8e8245dadb49f346af90
Andrés N. Robalino <andres@androbtech.com>
2021-11-17 12:04:44 -0500

    Datos: Casos (completo/MSP/2021-11-16) No. 628

--- spec/ecuacovid/criterios.rb
+++ spec/ecuacovid/criterios.rb
@@ -2522,7 +2522,8 @@ class Criterios
         [  :SNGRE_624_08H   ,"12/11/2021", {muestras: 1984250, rezagadas: 101462}],
         [  :SNGRE_625_08H   ,"13/11/2021", {muestras: 1987521, rezagadas: 101486}],
         [  :SNGRE_626_08H   ,"14/11/2021", {muestras: 1988664, rezagadas: 101515}],
-        [  :SNGRE_627_08H   ,"15/11/2021", {muestras: 1989376, rezagadas: 101599}]
+        [  :SNGRE_627_08H   ,"15/11/2021", {muestras: 1989376, rezagadas: 101599}],
+        [  :SNGRE_628_08H   ,"16/11/2021", {muestras: 1994091, rezagadas: 101345}]
       ]
       .reverse
       .seleccionable
