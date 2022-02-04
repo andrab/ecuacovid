ca69d72f47932fa8bd52d7d9bc0ca8ea885212e9
Andrés N. Robalino <andres@androbtech.com>
2022-02-04 11:23:39 -0500

    Datos: casos(completo/MSP/2022-03-02) No. 707

--- spec/ecuacovid/criterios.rb
+++ spec/ecuacovid/criterios.rb
@@ -2485,7 +2485,8 @@ class Criterios
         [  :SNGRE_703_08H   ,"30/01/2022", {muestras: 2486884, rezagadas: 135895}],
         [  :SNGRE_704_08H   ,"31/01/2022", {muestras: 2490742, rezagadas: 136063}],
         [  :SNGRE_705_08H   ,"01/02/2022", {muestras: 2502437, rezagadas: 135380}],
-        [  :SNGRE_706_08H   ,"02/02/2022", {muestras: 2512981, rezagadas: 135440}]
+        [  :SNGRE_706_08H   ,"02/02/2022", {muestras: 2512981, rezagadas: 135440}],
+        [  :SNGRE_707_08H   ,"03/02/2022", {muestras: 2523225, rezagadas: 135352}]
       ]
       .reverse
       .seleccionable
