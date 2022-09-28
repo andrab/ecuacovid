5a777b15a79e06c90dc86ad5aa25ba062ed5f71e
Andrés N. Robalino <andres@androbtech.com>
2022-09-28 02:24:38 -0500

    Datos: casos(completo/MSP/2022-08-09) No. 924

--- spec/ecuacovid/criterios.rb
+++ spec/ecuacovid/criterios.rb
@@ -3136,7 +3136,8 @@ class Criterios
         [  :SNGRE_920_08H   ,"04/09/2022", {muestras: 3381180, rezagadas: 247280}],
         [  :SNGRE_921_08H   ,"05/09/2022", {muestras: 3381510, rezagadas: 247280}],
         [  :SNGRE_922_08H   ,"06/09/2022", {muestras: 3382809, rezagadas: 247280}],
-        [  :SNGRE_923_08H   ,"07/09/2022", {muestras: 3384045, rezagadas: 247280}]
+        [  :SNGRE_923_08H   ,"07/09/2022", {muestras: 3384045, rezagadas: 247280}],
+        [  :SNGRE_924_08H   ,"08/09/2022", {muestras: 3385114, rezagadas: 247280}]
       ]
       .reverse
       .seleccionable
